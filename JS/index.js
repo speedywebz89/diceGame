e0dd7abb06413a125d4685fddd24127f9c9a3ef6
Tyler Gonzalez <106620557+speedywebz89@users.noreply.github.com>
2022-08-02 22:35:45 -0400

    init commit

---
+++ JS/index.js
@@ -0,0 +1,33 @@
+var button = document.querySelector(".btn");
+
+
+button.addEventListener("click", start);
+
+
+
+function start() {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+
+    document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png");
+    
+    document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+    if (randomNumber1 > randomNumber2) {
+        document.querySelector("h1").innerHTML = " Player 1 WINS!";
+    }
+
+    else if (randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = " Player 2 WINS!";
+    }
+
+    else {
+        document.querySelector("h1").innerHTML = " DRAW!";
+    }
+
+}
+
+
+
